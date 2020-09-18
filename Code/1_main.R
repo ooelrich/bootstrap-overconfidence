da1859810249ecb776a7cf0c922c907df829a820
Oscar Oelrich <oscar.oelrich@stat.su.se>
2020-09-18 17:43:03 +0200

    sudo reboot

--- Code/1_main.R
+++ Code/1_main.R
@@ -10,10 +10,10 @@ seed_val <- floor(runif(1) * 1e9)
 set.seed(seed_val)
 n_obs <- 1e2 # Sample size
 design_mat <- dgp(n_obs, 2, 2, trueMean = TRUE)
-n_parents <- 1e3 # no data sets to boostrap from
-n_bss <- 1e3 # no bootstrap replicates per parent
-sim_reps <- 1e5 # no reps to determine true sampling variance
-dfs <- c(3, 4, 5, 6) # degrees of freedom of the dgp
+n_parents <- 3e3 # no data sets to boostrap from
+n_bss <- 3e3 # no bootstrap replicates per parent
+sim_reps <- 1e6 # no reps to determine true sampling variance
+dfs <- c(3, 4, 5, 6, 7, 8, 9, 10) # degrees of freedom of the dgp
 sigma2 <- 0 # error variance of misspecified models, 0 means estimated freely
 
 ####################
