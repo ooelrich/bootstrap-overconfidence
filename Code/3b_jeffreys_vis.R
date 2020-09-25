382a2451ee6852f53e1e173cd40a089dee7c367c
Oscar Oelrich <oscar.oelrich@stat.su.se>
2020-09-25 18:01:28 +0200

    jeffreys with one f

--- Code/3b_jeffreys_vis.R
+++ Code/3b_jeffreys_vis.R
@@ -1,4 +1,7 @@
-is_radical <- function(x) { abs(x) > 5}
+# Dichotomize the data
+
+# The bootstrap data
+is_radical <- function(x) { abs(x) > 10}
 df_vals <- length(dfs_boot)
 data_shell <- matrix(NA, nrow = n_parents, ncol = df_vals)
 
@@ -7,14 +10,23 @@ for (i in seq_len(length(dfs_boot))) {
     data_jeff <- apply(data, c(1, 2), is_radical)
     summary_jeff <- apply(data_jeff, 2, sum) / n_bss
     data_shell[ , i] <- summary_jeff
-    print(i)
 }
 
 shell_melt <- melt(data_shell)[, 2:3]
 colnames(shell_melt) <- c("df", "radical")
 shell_melt$df <- as.factor(shell_melt$df)
 
-ggplot(shell_melt, aes(x = radical, color = df)) +
-    geom_boxplot(outlier.shape = NA)
+# The baseline data
+
+baseline_jeff <- is_radical(baseline_dat)
+summary_baseline_jeff <- colSums(baseline_jeff) / nrow(baseline_jeff)
+
+truth <- rep(summary_baseline_jeff, each  = n_parents)
+shell_melt <- cbind(shell_melt, truth)
+# Plotting it all
+
+ggplot(shell_melt, aes(x = radical)) +
+    geom_density() + geom_point(aes(x = truth, y = 0), col = "red") + facet_grid(rows = vars(df))
+
 
 head(shell_melt)
