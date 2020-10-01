204a15bf85f8488bda76ba6daa93f9dfb41dbac0
Oscar Oelrich <oscar.oelrich@stat.su.se>
2020-10-01 13:18:55 +0200

    three different size

--- Code/3b_jeffreys_vis.R
+++ Code/3b_jeffreys_vis.R
@@ -1,7 +1,7 @@
 # Dichotomize the data
 
 # The bootstrap data
-is_radical <- function(x) { abs(x) > 5 }
+is_radical <- function(x) { abs(x) > 10 }
 df_vals <- length(dfs_boot)
 data_shell <- matrix(NA, nrow = n_parents, ncol = df_vals)
 
@@ -37,7 +37,7 @@ for (i in seq_len(length(dfs))) {
 
 shell_melt$df <- as.factor(shell_melt$df)
 
-pdf("n50_jeff5.pdf")
+jpeg("n1000_jeff10.jpg", width = 700, height = 700)
 ggplot(shell_melt, aes(x = radical)) +
     geom_density() + geom_point(aes(x = truth, y = 0.1), col = "red") +
     geom_point(aes(x = mean_theta, y = 0.1), col = "black") +
@@ -45,6 +45,6 @@ ggplot(shell_melt, aes(x = radical)) +
     xlab("Probability of observing conclusive evidence") + ylab("Density") +
     #theme_tufte() + #theme(axis.line = element_line()) +
     scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) +
-    scale_y_continuous(expand = c(0, 0), limits = c(0, 3)) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, 10)) +
     theme_bw() + theme(text = element_text(size = 15))
 dev.off()
