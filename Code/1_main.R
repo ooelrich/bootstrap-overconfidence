9ac3ae79482bd1cc9d0eea18d1b1a0a23fb781c9
Oscar Oelrich <oscar.oelrich@stat.su.se>
2020-10-04 16:55:55 +0200

    jack the knife

--- Code/1_main.R
+++ Code/1_main.R
@@ -11,8 +11,8 @@ set.seed(seed_val)
 n_obs <- 1e2 # Sample size
 design_mat <- dgp(n_obs, c(1, 1), true_mean = TRUE)
 n_parents <- 1e4 # no data sets to boostrap from
-n_bss <- 1e2 # no bootstrap replicates per parent
-sim_reps <- 1e3 # no reps to determine true sampling variance
+n_bss <- 1e4 # no bootstrap replicates per parent
+sim_reps <- 1e6 # no reps to determine true sampling variance
 dfs <- c(2.5, 5, 30) # degrees of freedom of the dgp
 sigma2 <- 0 # error variance of misspecified models, 0 means estimated freely
 
